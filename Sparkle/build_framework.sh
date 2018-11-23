3e2ba2f4db7e741ca50e9b6fc71716d2a895f427
Nickey Borislov <n.borislov@synergysportstech.com>
2018-11-23 16:38:40 +0200

    updating sparkle sharp to use the latest

--- Sparkle/build_framework.sh
+++ Sparkle/build_framework.sh
@@ -2,6 +2,6 @@
 
 DIR=$(dirname ${0})
 cd ${DIR}
-git submodule init && git submodule update
+# git submodule init && git submodule update
 cd 3rdparty/Sparkle
 xcodebuild -configuration Release -target Sparkle
